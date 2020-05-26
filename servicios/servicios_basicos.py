98f03c1e8a3cb63ebb92ff25b158d44cd9ffb9ff
Muerterauda <juanpalmaborda@hotmail.com>
2020-05-26 17:36:20 +0200

    Conexion momentanea con MongoDB

--- servicios/servicios_basicos.py
+++ servicios/servicios_basicos.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
+from mongo.mongo_manager import granja_colection
 
 granja_bp = Blueprint('medidores_granja', __name__, template_folder='templates')
 
@@ -6,4 +7,5 @@ granja_bp = Blueprint('medidores_granja', __name__, template_folder='templates')
 @granja_bp.route("/prueba", methods=['GET'])
 def metodo_prueba():
     dato = request.args.get("dato")
-    return "Si"
+    granja_colection.insert({"dato": dato})
+    return render_template("inicio.html")
